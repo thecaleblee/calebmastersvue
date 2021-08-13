fb4acf4bb3c87599557541d21c9ec5d84feeff03
Caleb Masters <calebmasters@gmail.com>
2021-08-13 12:27:48 -0500

    Adding scss parser & nav component

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -19,7 +19,7 @@
           </a>
         </li>
         <li>
-          <a href="#skills" aria-label="Skills">
+          <a href="#skills" aria-label="Skills" @click="scrollTo">
             <i class="gg-terminal"></i>
             Skills
           </a>
@@ -34,8 +34,7 @@
       <ul class="tech" aria-hidden="true">
         <li>
           <a
-            href="/"
-            class="active"
+            href="https://calebmasters.dev"
             aria-label="Change Website Tech Stack to: HTML/CSS/JS"
             >HTML/CSS/JS</a
           >
@@ -54,6 +53,16 @@
             >Angular.js/SCSS</a
           >
         </li>
+        <li>
+          <a
+            href="/"
+            aria-disabled="true"
+            class="active"
+            @click.prevent
+            aria-label="Change Website Tech Stack to: Vue.js"
+            >Vue.js</a
+          >
+        </li>
       </ul>
     </nav>
   </header>
@@ -70,74 +79,296 @@ export default defineComponent({
 });
 </script>
 
-<style>
+<style lang="scss" scoped>
 nav {
-	display: flex;
-	flex-flow: column wrap;
-	justify-content: space-between;
-	padding-top: 1rem;
+  display: flex;
+  flex-flow: column wrap;
+  justify-content: space-between;
+  padding-top: 1rem;
+
+  img {
+    max-height: 3rem;
+    margin-bottom: .2rem;
+  }
+
+  ul {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: flex-start;
+    margin: 0;
+  }
+
+  .tech {
+    font-size: 1rem;
+    color: var(--main-footer-link);
+    background: var(--secondary-text);
+    justify-content: space-evenly;
+    padding: 0.3rem 0;
+
+    a {
+      color: var(--tertiary-bg);
+      text-decoration: none;
+
+      &.active,
+      &:hover {
+        border-bottom: 1px dotted var(--main-bg);
+      }
+    }
+  }
+
+  ul:first-child {
+    i {
+      margin-right: 1rem;
+    }
+
+    a {
+      font-family: "Montserrat";
+      color: var(--main-text);
+      display: flex;
+      flex-direction: column-reverse;
+      align-items: center;
+
+      &:visited {
+        font-family: "Montserrat";
+        color: var(--main-text);
+      }
+
+      &.active,
+      &:hover {
+        color: var(--main-theme);
+        text-decoration: none;
+
+        i {
+          color: var(--main-theme);
+
+          &.gg-browser {
+            &::before,
+            &::after {
+              background: var(--main-theme);
+            }
+          }
+          &.gg-terminal::after {
+            background: var(--main-theme);
+          }
+        }
+      }
+    }
+  }
 }
 
-nav img {
-	max-height: 3rem;
+.gg-browser {
+  box-sizing: border-box;
+  position: relative;
+  display: block;
+  transform: scale(var(--ggs, 0.7));
+  width: 24px;
+  height: 18px;
+  border: 2px solid;
+  border-radius: 3px;
+  color: var(--main-text);
+
+  &::after,
+  &::before {
+    content: "";
+    display: block;
+    box-sizing: border-box;
+    position: absolute;
+    width: 22px;
+    height: 2px;
+    background: var(--main-text);
+    top: 4px;
+  }
+
+  &::after {
+    border-radius: 3px;
+    width: 2px;
+    box-shadow: 3px 0 0, 6px 0 0;
+    top: 1px;
+    left: 1px;
+  }
 }
 
-nav ul {
-	display: flex;
-	flex-direction: row;
-	justify-content: space-around;
-	align-items: flex-start;
-	margin: 0;
+.gg-briefcase {
+  color: var(--main-text);
+  box-sizing: border-box;
+  position: relative;
+  display: block;
+  transform: scale(var(--ggs, 0.8));
+  width: 22px;
+  height: 16px;
+  border: 2px solid;
+  border-radius: 2px;
+  margin-top: 1px;
+
+  &::after {
+    content: "";
+    display: block;
+    box-sizing: border-box;
+    position: absolute;
+  }
+
+  &::before {
+    content: "";
+    display: block;
+    box-sizing: border-box;
+    position: absolute;
+    border: 2px solid;
+    border-top-left-radius: 1px;
+    border-top-right-radius: 1px;
+    left: 4px;
+    width: 10px;
+    height: 4px;
+    border-bottom: 0;
+    top: -5px;
+  }
+
+  &::after {
+    width: 18px;
+    height: 3px;
+    border-left: 7px solid transparent;
+    border-right: 7px solid transparent;
+    border-bottom: 1px solid transparent;
+    box-shadow: 0 2px 0, inset 16px 74px 0;
+    top: 4px;
+  }
 }
 
-nav .tech {
-	font-size: 1rem;
-	color: var(--main-footer-link);
-	background: var(--secondary-text);
-	justify-content: space-evenly;
-	padding: 0.3rem 0;
+.gg-terminal {
+  color: var(--main-text);
+  box-sizing: border-box;
+  position: relative;
+  display: block;
+  transform: scale(var(--ggs, 0.7));
+  width: 24px;
+  height: 20px;
+  border: 2px solid;
+  border-radius: 2px;
+
+  &::after {
+    content: "";
+    display: block;
+    box-sizing: border-box;
+    position: absolute;
+  }
+
+  &::before {
+    content: "";
+    display: block;
+    box-sizing: border-box;
+    position: absolute;
+    border-right: 2px solid;
+    border-bottom: 2px solid;
+    transform: rotate(-45deg);
+    width: 6px;
+    height: 6px;
+    top: 5px;
+    left: 3px;
+  }
+
+  &::after {
+    width: 4px;
+    height: 2px;
+    background: var(--main-text);
+    top: 10px;
+    left: 11px;
+  }
 }
 
-nav .tech a {
-	color: var(--tertiary-bg);
-	text-decoration: none;
+.gg-link {
+  box-sizing: border-box;
+  position: relative;
+  display: inline-block;
+  transform: rotate(-45deg) scale(var(--ggs, 1));
+  width: 8px;
+  height: 2px;
+  background: currentColor;
+  border-radius: 4px;
+  vertical-align: middle;
+  margin-left: 0.5rem;
+
+  &::after,
+  &::before {
+    content: "";
+    display: block;
+    box-sizing: border-box;
+    position: absolute;
+    border-radius: 3px;
+    width: 8px;
+    height: 10px;
+    border: 2px solid;
+    top: -4px;
+  }
+
+  &::before {
+    border-right: 0;
+    border-top-left-radius: 40px;
+    border-bottom-left-radius: 40px;
+    left: -6px;
+  }
+
+  &::after {
+    border-left: 0;
+    border-top-right-radius: 40px;
+    border-bottom-right-radius: 40px;
+    right: -6px;
+  }
 }
 
-nav .tech a.active, nav .tech a:hover {
-	border-bottom: 1px dotted var(--main-bg);
+@media screen and (min-width: 415px) {
+  nav ul:first-child li a {
+    flex-direction: row;
+  }
 }
 
-nav ul:first-child i {
-	margin-right: 1rem;
+@media screen and (min-width: 567px) {
+  nav {
+    background: var(--main-bg);
+    box-shadow: 0 0 3px rgb(0 0 0 / 30%);
+    flex-direction: column-reverse;
+    padding-top: 0;
+    padding-bottom: .5rem;
+    position: fixed;
+    width: 100%;
+    z-index: 9999;
+
+    img {
+      margin-bottom: 0;
+      margin-top: .2rem;
+    }
+
+    &.tech {
+    margin-bottom: 1rem;
+    justify-content: flex-end;
+
+      a {
+        margin-right: 2rem;
+      }
+    }
+  }
 }
 
-nav ul:first-child a {
-	font-family: "Montserrat";
-	color: var(--main-text);
-	display: flex;
-	flex-direction: column-reverse;
-	align-items: center;
+@media screen and (min-width: 784px) {
+  nav ul:first-child li {
+    font-size: 2rem;
+  }
 }
 
-nav ul:first-child a:visited {
-	font-family: "Montserrat";
-	color: var(--main-text);
+@media screen and (min-width: 905px) {
+  nav img {
+    justify-self: flex-start; 
+  }
 }
 
-nav ul:first-child a.active, nav ul:first-child a:hover {
-	color: var(--main-theme);
-	text-decoration: none;
+@media screen and (min-width: 1100px) {
+  nav ul:first-child li {
+    font-size: 2.2rem;
+  }
 }
 
-nav ul:first-child a.active i, nav ul:first-child a:hover i {
-	color: var(--main-theme);
-}
-
-nav ul:first-child a.active i.gg-browser::before, nav ul:first-child a.active i.gg-browser::after, nav ul:first-child a:hover i.gg-browser::before, nav ul:first-child a:hover i.gg-browser::after {
-	background: var(--main-theme);
-}
-
-nav ul:first-child a.active i.gg-terminal::after, nav ul:first-child a:hover i.gg-terminal::after {
-	background: var(--main-theme);
+@media screen and (min-width: 1640px) {
+  nav ul:first-child li {
+    font-size: 2.4rem;
+  }
 }
 </style>
